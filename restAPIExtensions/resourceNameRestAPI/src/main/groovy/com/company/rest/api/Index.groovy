163cf9c1fe31e52afc4d35443f522033d94f0501
oran2527 <orlago250183@gmail.com>
2023-06-07 17:50:13 -0500

    cambios en groovy h2

--- restAPIExtensions/resourceNameRestAPI/src/main/groovy/com/company/rest/api/Index.groovy
+++ restAPIExtensions/resourceNameRestAPI/src/main/groovy/com/company/rest/api/Index.groovy
@@ -1,14 +1,17 @@
 package com.company.rest.api;
 
-@GrabConfig(systemClassLoader=true)
-@Grab(group='com.h2database', module='h2', version='1.4.199')
+
 
 
 import com.company.model.Claim2
 import groovy.json.JsonBuilder
 import groovy.sql.Sql
 
-import java.sql.*
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -32,80 +35,28 @@ class Index implements RestApiController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Index.class)
 
+	private Connection conn;
+	private Statement stmt;
+	
     @Override
     RestApiResponse doHandle(HttpServletRequest request, RestApiResponseBuilder responseBuilder, RestAPIContext context) {
-        // To retrieve query parameters use the request.getParameter(..) method.
-        // Be careful, parameter values are always returned as String values
-		
-		/*
-        // Retrieve p parameter
-        def p = request.getParameter "p"
-		p = 1
-        if (p == null) {
-            return buildResponse(responseBuilder, HttpServletResponse.SC_BAD_REQUEST,"""{"error" : "the parameter p is missing"}""")
-        }
-
-        // Retrieve c parameter
-        def c = request.getParameter "c"
-		c = 2
-        if (c == null) {
-            return buildResponse(responseBuilder, HttpServletResponse.SC_BAD_REQUEST,"""{"error" : "the parameter c is missing"}""")
-        }
-		
-		// ***** own code start ******
 		
 		
-		// use APIClient to retrieve the CarDAO class:
-		def claim2DAO = context.apiClient.getDAO(Claim2.class)
-		
-		def currentModel = "claim2"
-		
-		List<Claim2> claim2s = claim2DAO.findByModel(currentModel, p as int, c as int)
-		
-		
-		def claim2Models = [:]
-		for (Claim2 claim2 : claim2s) {
-			// return the fields "id", "buildYear", "color":
-			claim2Models.put("${claim2.description2} - ${claim2.answer2} - ${claim2.satisfactionLevel2}")
-		}
-
-		
-		
-        // Here is an example of how you can retrieve configuration parameters from a properties file
-        // It is safe to remove this if no configuration is required
-        // Properties props = loadProperties("configuration.properties", context.resourceProvider)
-        // String paramValue = props["myParameterKey"]
-		
-		
-		//def result = [ "model" : currentModel, "number of claims" : claim2s.size(), "claims" : claim2Models ]
+		LOGGER.info("Connecting to database... punto 1");
+		// STEP 1: Register JDBC driver
+		Class.forName("org.h2.Driver");
+		LOGGER.info("Connecting to database... punto 2");
+		LOGGER.info("Connecting to database... punto 2.1");
+		conn = DriverManager.getConnection("jdbc:h2:file:C:/BonitaStudioCommunity-2021.1/workspace/Mi Proyecto/h2_database/business_data.db","sa","");
+		LOGGER.info("Connecting to database... punto 3");
+		stmt = conn.createStatement();
+		LOGGER.info("Connecting to database... punto 4");
+		String sql = "select * from claim2"
+		stmt.execute(sql);
+		LOGGER.info("Connecting to database... punto 5");
+        conn.close()
         
-		
-		//def result = ["id": 2, "texto": "hola"]
-		
-		
-
-		
-        // ***** own code start ******
-		
-		
-        // def result = [ "p" : p ,"c" : c , "myParameterKey" : paramValue, "currentDate" : LocalDate.now().toString() ]
-
-        // Send the result as a JSON representation
-        // You may use buildPagedResponse if your result is multiple
-        return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result).toString())
-        
-        */
-		
-		Connection conn = DriverManager.getConnection("jdbc:h2:~/bomt", "sa", "");
-		// add application code here
-		Statement stat = conn.createStatement();
-		ResultSet rs;
-		rs = stat.executeQuery("select * from claim2");
-		
-		while (rs.next()) {
-			System.out.println(rs.getString("description2"));
-		}
-		conn.close();
+	
 		
 		
 		def result = ["id": 2, "texto": "hola"]
